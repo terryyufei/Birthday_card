9996023cc6789126c31e76692b08d11decdc264f
Terry Wambui <shikuwambui@gmail.com>
2023-12-08 20:22:53 +0300

    for loveth

---
+++ script.js
@@ -0,0 +1,10 @@
+// JavaScript functions to play and pause the background music
+var backgroundMusic = document.getElementById("backgroundMusic");
+
+function playMusic() {
+    backgroundMusic.play();
+}
+
+function pauseMusic() {
+    backgroundMusic.pause();
+}
